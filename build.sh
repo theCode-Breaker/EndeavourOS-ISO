6794457320ded43e72e97b8faeb6a5d86ec8eb3f
Adarsh <83777943+theCode-Breaker@users.noreply.github.com>
2022-02-20 18:27:50 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 pacman -S --noconfirm --needed archlinux-keyring
-pacman -S --needed --noconfirm archiso mkinitcpio-archiso
 pacman -S wget --noconfirm --needed
 pacman-key --init && pacman-key --recv-key 003DB8B0CB23504F --keyserver keyserver.ubuntu.com && pacman-key --lsign-key 003DB8B0CB23504F
 wget https://raw.githubusercontent.com/endeavouros-team/EndeavourOS-ISO/main/airootfs/etc/pacman.conf -O /etc
